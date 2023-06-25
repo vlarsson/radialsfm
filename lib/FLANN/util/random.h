fe294ca2efb115174df00457882ce291ae471921
timduff35@github.com <timothy.duff@ncf.edu>
2023-06-25 15:37:20 -0700

    mac m1 compilation

--- lib/FLANN/util/random.h
+++ lib/FLANN/util/random.h
@@ -35,6 +35,7 @@
 #include <cstdlib>
 #include <cstddef>
 #include <vector>
+#include <random>
 
 #include "FLANN/general.h"
 
@@ -117,7 +118,9 @@ public:
         for (int i = 0; i < size_; ++i) vals_[i] = i;
 
         // shuffle the elements in the array
-        std::random_shuffle(vals_.begin(), vals_.end(), generator);
+	std::random_device rd;
+	std::mt19937 g(rd());
+        std::shuffle(vals_.begin(), vals_.end(), g);
 
         counter_ = 0;
     }
